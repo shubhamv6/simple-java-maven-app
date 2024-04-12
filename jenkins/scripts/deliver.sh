c21e911a85e0bf03aecb8af19f560cbcd216de22
shubhamv6 <31300897+shubhamv6@users.noreply.github.com>
2024-04-12 09:23:06 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo 'The following Maven command installs your Maven-built Java application into the local Maven repository, which will ultimately be stored in Jenkins\'s local Maven repository (and the "maven-repository" Docker data volume).'
+echo 'The following Maven command installs your Maven-built Java application into the local Maven repository, which will ultimately be stored in Jenkins\'s local Maven repository \(and the "maven-repository" Docker data volume\).'
 set -x
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 set +x
